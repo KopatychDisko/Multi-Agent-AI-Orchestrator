15915b14836276f9146d67f50ccd3c0ff3784770
KopatychDisko <95695789+KopatychDisko@users.noreply.github.com>
2025-08-11 12:31:48 +0300

    Done

--- app/team_agents.py
+++ app/team_agents.py
@@ -1,9 +1,11 @@
 from langchain_core.messages import HumanMessage
+from langchain_core.runnables import RunnableConfig
 
 from langgraph.graph import StateGraph
 from langgraph.constants import END
-from app.schema import State, members
+from langgraph.checkpoint.memory import MemorySaver
 
+from app.schema import State, members
 from app.agents import supervisor_node, files_node, coder_node, research_node, answer_node
 
 
@@ -30,12 +32,14 @@ graph.set_entry_point('supervisor')
 
 graph.add_edge('answer_agent', END)
 
-app = graph.compile()
+memory = MemorySaver()
+app = graph.compile(checkpointer=memory)
 
 def invoke_graph(message: str):
+    config = RunnableConfig(recursion_limit=10, configurable={"thread_id": "1"})
     temp_state = State(messages=HumanMessage(message))
 
-    for step in app.stream(temp_state, stream_mode='values'):
+    for step in app.stream(temp_state, stream_mode='values', config=config):
         for k, v in step.items():
             if k != "__end__":
                 print('==============')
